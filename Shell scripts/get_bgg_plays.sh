74b9cb2c3776affc02468674416934b5a6699510
Andreas Pettersson <psson@users.noreply.github.com>
2020-02-20 22:33:01 +0100

    Parameters added to play downloads
    Added and tested -d parameter in scripts that download data for plays

--- Shell scripts/get_bgg_plays.sh
+++ Shell scripts/get_bgg_plays.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+
+while getopts ":d" opt ; do
+	case $opt in
+		d) download_dir="$OPTARG"
+		;;
+		\?) echo "Invalid option -$OPTARG" >&2
+		;;
+	esac
+done
+
+cd $download_dir
 num_plays=$(xmllint --xpath 'string(/plays/@total)' ./plays_1.xml)
 num_pages=$((num_plays/100+1))
 p=2
 # Get the remaining pages
 while [ $p -le $num_pages ]
 do
-wget -O "plays_$p.xml" "https://www.boardgamegeek.com/xmlapi2/plays?username=psson73&mindate=2020-01-01&maxdate=2020-12-31&page=$p"
+	wget -O "plays_$p.xml" "https://www.boardgamegeek.com/xmlapi2/plays?username=psson73&mindate=2020-01-01&maxdate=2020-12-31&page=$p"
 p=$((p+1))
 done
--- Shell scripts/get_bgg_plays_page1.sh
+++ Shell scripts/get_bgg_plays_page1.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+while getopts ":d" opt ; do
+	case $opt in
+		d) download_dir="$OPTARG"
+		;;
+		\?) echo "Invalid option -$OPTARG" >&2
+		;;
+	esac
+done
+
+cd $download_dir
 wget "https://www.boardgamegeek.com/xmlapi2/plays?username=psson73&mindate=2020-01-01&maxdate=2020-12-31&page=1" -O plays_1.xml

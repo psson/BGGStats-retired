02a435179975174366d65200e2e5b9e9e34df6fd
Andreas Pettersson <psson@users.noreply.github.com>
2020-02-22 19:49:26 +0100

    Update combine_plays.sh
    Updated script to take parameters for download directory and web server root

--- Shell scripts/combine_plays.sh
+++ Shell scripts/combine_plays.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-upload_dir='/home/pi/xml'
+
+while getopts ":d:w" opt ; do
+	case $opt in
+		d) download_dir="$OPTARG"
+		;;
+		w) www_dir="$OPTARG"
+		;;
+		\?) echo "Invalid option -$OPTARG" >&2
+		;;
+	esac
+done
+
+download_dir='/home/pi/xml'
 www_dir='/var/www/html'
-cd $upload_dir
+cd $download_dir
 file_name='combined_plays'
 file_xml="$file_name.xml"
 xml_grep --pretty_print indented --wrap plays --descr --cond "plays" plays_*.xml > "$file_xml"
 sed -i "s/?>/?><?xml-stylesheet type=\"text\/xsl\" href=\"conf\/combined_plays.xsl\"?>/" "$file_xml"
-sudo cp "$upload_dir/$file_xml" "$www_dir"
+sudo cp "$download_dir/$file_xml" "$www_dir"

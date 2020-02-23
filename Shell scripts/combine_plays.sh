27af7bc0381b871ad5ccd692eb45d31d7b86216e
Andreas Pettersson <psson@users.noreply.github.com>
2020-02-23 18:51:47 +0100

    Update combine_plays.sh
    Uncommented line that removes the downloaded play files from the working directory

--- Shell scripts/combine_plays.sh
+++ Shell scripts/combine_plays.sh
@@ -12,10 +12,10 @@ while getopts ":d:w:" opt ; do
 done
 
 cd $download_dir
-pwd
+
 file_name='combined_plays'
 file_xml="$file_name.xml"
 xml_grep --pretty_print indented --wrap plays --descr --cond "plays" plays_*.xml > "$file_xml"
 sed -i "s/?>/?><?xml-stylesheet type=\"text\/xsl\" href=\"conf\/combined_plays.xsl\"?>/" "$file_xml"
 sudo cp "$download_dir/$file_xml" "$www_dir"
-#rm plays_*.xml
+rm plays_*.xml

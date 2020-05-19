124f49fcc6a471f2ee982b10e2c6ae5f60c98561
Andreas Pettersson <psson@users.noreply.github.com>
2020-05-19 18:36:19 +0200

    Small changes to initial files

--- Shell scripts/combine_plays.sh
+++ Shell scripts/combine_plays.sh
@@ -14,8 +14,13 @@ done
 cd $download_dir
 
 file_name='combined_plays'
+file_name2='my2020_10x10'
 file_xml="$file_name.xml"
+file2_xml="$file_name2.xml"
 xml_grep --pretty_print indented --wrap plays --descr --cond "plays" plays_*.xml > "$file_xml"
+cp "$file_xml" "file_xml"
 sed -i "s/?>/?><?xml-stylesheet type=\"text\/xsl\" href=\"conf\/combined_plays.xsl\"?>/" "$file_xml"
+sed -i "s/?>/?><?xml-stylesheet type=\"text\/xsl\" href=\"conf\/my2020_10x10.xsl\"?>/" "$file2_xml"
 sudo mv "$download_dir/$file_xml" "$www_dir"
+sudo mv "$download_dir/$file2_xml" "$www_dir"
 rm plays_*.xml
---
+++ site root/conf/my2020_10x10.xsl
@@ -0,0 +1,2 @@
+for $x in doc("my2020_10x10.xml")/plays/play
+where $x[item[@objectid='146021']]
